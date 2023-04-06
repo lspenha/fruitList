9db26bae0fce44a37e3aedc8be8bfce743fee6d6
Lucas Penha <lspenhacontato@gmail.com>
2023-04-06 01:58:36 -0300

    feat: indentation

--- src/components/CardDetail/CardDetail.js
+++ src/components/CardDetail/CardDetail.js
@@ -17,15 +17,15 @@ function CardDetail () {
       <div className="main">
         <h1>Detalhes da fruta</h1>
           {fruit &&    
-              <div className="card">
-                <div className="card_image"><img src={fruit.photo} alt="Fruit"/></div>
-                <div className="card_content">
-                  <h2 className="card_title">{fruit.name}</h2>
-                  <p className="card_text">{`Calorias: ${fruit.calories} | Proteína: ${fruit.protein} | Carboidrato: ${fruit.carbohydrates}`}</p>
-                  <p className="card_text">{`Fibra: ${fruit.fiber} | Gordura ${fruit.blubber} | Porção: ${fruit.portion}`}</p>
-                  <Link to="/" className="btn card_btn">retornar a página inicial</Link>
-                </div>
+            <div className="card">
+              <div className="card_image"><img src={fruit.photo} alt="Fruit"/></div>
+              <div className="card_content">
+                <h2 className="card_title">{fruit.name}</h2>
+                <p className="card_text">{`Calorias: ${fruit.calories} | Proteína: ${fruit.protein} | Carboidrato: ${fruit.carbohydrates}`}</p>
+                <p className="card_text">{`Fibra: ${fruit.fiber} | Gordura ${fruit.blubber} | Porção: ${fruit.portion}`}</p>
+                <Link to="/" className="btn card_btn">retornar a página inicial</Link>
               </div>
+            </div>
           }
       </div>
     </>
